d4f611db0f239364b9204860714a9c9bc52627e6
Szczurovsky <dszczurkowski@gmail.com>
2021-06-29 19:44:53 +0200

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-<<<<<<< HEAD
 (1..100).each {|i| Meme.create(name: i, file: "./public/pictures/#{i}.png")}
-p "Created #{Meme.count} memes"
-=======
-(1..100).each {|i| Mem.create(name: i, file: "./public/pictures/#{i}.png")}
-p "Created #{Mem.count} memes"
->>>>>>> cf1a89de53f9a93fcd80b1c570d46e65304c0b75
+p "Created #{Meme.count} memess"
